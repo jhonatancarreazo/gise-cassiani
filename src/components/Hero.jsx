32ae7b79b290c8561553f20a136ea680c582200b
Jhonatan Carreazo <jhonatan.carreazoa@gmail.com>
2025-12-18 16:12:40 -0500

    fix: mobile hero & gallery layout + video background

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,15 +8,27 @@ function Hero () {
         
 <>
   <div className="layer">
-    <video autoPlay loop muted playsInline>
+    <video autoPlay
+  muted
+  loop
+  playsInline
+  preload="auto">
       <source src="/videos/video1.mp4" type="video/mp4" />
     </video>
 
-    <video autoPlay loop muted playsInline>
+    <video autoPlay
+  muted
+  loop
+  playsInline
+  preload="auto">
       <source src="/videos/video2.mp4" type="video/mp4" />
     </video>
 
-    <video autoPlay loop muted playsInline>
+    <video autoPlay
+  muted
+  loop
+  playsInline
+  preload="auto">
       <source src="/videos/video3.mp4" type="video/mp4" />
     </video>
   </div>
@@ -36,7 +48,7 @@ function Hero () {
     </div>
       <div className="hero-image"><img alt="Gise Cassiani mom life" src="https://scontent.fctg3-1.fna.fbcdn.net/v/t51.75761-15/503026976_18511885459011112_2456021382755067186_n.jpg?stp=dst-jpegr_tt6&amp;_nc_cat=107&amp;ccb=1-7&amp;_nc_sid=127cfc&amp;_nc_eui2=AeEJ5tXMD-n3V9BIS-ztU80rF9k1qYMoCCYX2TWpgygIJgUkuuZP88f-YH3IKpjZo5Q&amp;_nc_ohc=Zje1Ehq4kSgQ7kNvwG0SACr&amp;_nc_oc=AdlqIrtq3ppjXYlmYpKWI8tEPPYNf-Zu9GfLUAcRdyX_sLE4BKFoMyeKhp075-ZJnHE&amp;_nc_zt=23&amp;se=-1&amp;_nc_ht=scontent.fctg3-1.fna&amp;_nc_gid=ptJI0P4zEsAsw2rT-fEWlg&amp;oh=00_AfmQml0ncFbPvBjseEDhQUqyEDAn9_atL4_vonfWXRqfjQ&amp;oe=694A1FDA" /></div>
   </section>
-  <img data-visualcompletion="media-vc-image" height="1800" class="x15mokao x1ga7v0g x16uus16 xbiv7yw x1bwycvy x193iq5w x4fas0m x19kjcj4" alt="No hay ninguna descripción de la foto disponible." referrerpolicy="origin-when-cross-origin" src=""></img>
+  
 </>
 
     )
